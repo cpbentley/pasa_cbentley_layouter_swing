8652d20687c609647aad6a91e46c8685a71444de
Charles Bentley <cbentley@mail.ru>
2019-11-29 18:53:53 +0100

    license + headers + jautodoc settings

--- src/pasa/cbentley/layouter/swing/engine/DragPozerListener.java
+++ src/pasa/cbentley/layouter/swing/engine/DragPozerListener.java
@@ -1,3 +1,7 @@
+/*
+ * (c) 2018-2019 Charles-Philip Bentley
+ * This code is licensed under MIT license (see LICENSE.txt for details)
+ */
 package pasa.cbentley.layouter.swing.engine;
 
 import java.awt.Point;
--- src/pasa/cbentley/layouter/swing/widgets/BLPanel.java
+++ src/pasa/cbentley/layouter/swing/widgets/BLPanel.java
@@ -1,3 +1,7 @@
+/*
+ * (c) 2018-2019 Charles-Philip Bentley
+ * This code is licensed under MIT license (see LICENSE.txt for details)
+ */
 package pasa.cbentley.layouter.swing.widgets;
 
 import java.awt.Component;
