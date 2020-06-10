1e2e72dccbf759e72d364be9b53c805d163df743
Charles Bentley <cbentley@mail.ru>
2020-06-10 15:20:52 +0200

    delegate demo fix

--- src/pasa/cbentley/layouter/swing/demo/RunLayouterDemoSwingDelegate.java
+++ src/pasa/cbentley/layouter/swing/demo/RunLayouterDemoSwingDelegate.java
@@ -5,7 +5,9 @@
 package pasa.cbentley.layouter.swing.demo;
 
 import java.awt.Color;
+import java.util.Random;
 
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
 import pasa.cbentley.byteobjects.src4.core.ByteObject;
@@ -13,12 +15,16 @@ import pasa.cbentley.core.src4.ctx.ConfigUSettable;
 import pasa.cbentley.core.src4.ctx.IConfigU;
 import pasa.cbentley.core.src4.ctx.UCtx;
 import pasa.cbentley.core.src4.logging.Dctx;
+import pasa.cbentley.layouter.src4.engine.Area2DConfigurator;
 import pasa.cbentley.layouter.src4.engine.LayoutDelegateAdapter;
 import pasa.cbentley.layouter.src4.engine.LayoutWillListenerAdapter;
 import pasa.cbentley.layouter.src4.interfaces.ILayoutDelegate;
 import pasa.cbentley.layouter.src4.interfaces.ILayoutable;
 import pasa.cbentley.layouter.src4.tech.ITechLayout;
+import pasa.cbentley.layouter.swing.engine.ConfiguratorForSwing;
+import pasa.cbentley.layouter.swing.engine.DragPozerListener;
 import pasa.cbentley.layouter.swing.engine.JPanelLayoutable;
+import pasa.cbentley.layouter.swing.engine.KeyListenerF4DebugStringable;
 import pasa.cbentley.layouter.swing.engine.LayoutableAdapterForJComponent;
 
 public class RunLayouterDemoSwingDelegate extends RunLayouterDemoSwingAbstract {
@@ -47,47 +53,98 @@ public class RunLayouterDemoSwingDelegate extends RunLayouterDemoSwingAbstract {
 
    private LayoutableAdapterForJComponent panelYellow;
 
+   private Random                         rnd;
+
    public RunLayouterDemoSwingDelegate(IConfigU configU) {
       super(configU);
 
       constructHelpers();
 
+      rnd = slc.getUCtx().getRandom();
    }
 
    public void buildDemo(JPanelLayoutable panel) {
 
+      ConfiguratorForSwing configurator = panel.getConfigurator();
+
       ILayoutDelegate delegate = new LayoutDelegateAdapter(slc) {
-
-         public ILayoutable getDelegateSizer(ByteObject sizer, ILayoutable layoutable, int ctx) {
-            if (ctx == ITechLayout.CTX_1_WIDTH) {
-               //when computing
-            }
-            if (layoutable == panelOrange) {
-
-            } else if (layoutable == panelBlue) {
-
-            }
-            // TODO Auto-generated method stub
-            return null;
-         }
 
          public int getDelegateSizeHeight(ByteObject sizer, ILayoutable layoutable) {
             if (layoutable == panelOrange) {
                int h = slc.getUCtx().getRandom().nextInt(300) + 10;
                return h;
+            } else if (layoutable == panelGreen) {
+               int currentH = layoutable.getSizeDrawnHeight();
+               int multi = 1;
+               if (currentH > 300) {
+                  multi = -1;
+               }
+               int h = currentH + (multi * rnd.nextInt(300)) + 10;
+               return h;
+            } else {
+               return rnd.nextInt(100) + 20;
             }
-            return 0;
          }
 
+         public int getDelegateSizeWidth(ByteObject sizer, ILayoutable layoutable) {
+            if (layoutable == panelOrange) {
+               int h = rnd.nextInt(300) + 10;
+               return h;
+            } else if (layoutable == panelGreen) {
+               int currentWidth = layoutable.getSizeDrawnWidth();
+               int multi = 1;
+               if (currentWidth > 300) {
+                  multi = -1;
+               }
+               int w = currentWidth + (multi * rnd.nextInt(300)) + 10;
+               return w;
+            } else if (layoutable == panelRed) {
+               int currentWidth = layoutable.getSizeDrawnWidth();
+               int multi = 1;
+               if (currentWidth > 300) {
+                  multi = -1;
+               }
+               if (currentWidth < 50) {
+                  currentWidth += 50;
+               }
+               int w = currentWidth + (multi * rnd.nextInt(20));
+               return w;
+            } else {
+               return rnd.nextInt(100) + 20;
+            }
+         }
       };
 
+      
       createOrange(panel, delegate);
-      createBlue(panel);
-      createGreen(panel);
-      createRed(panel);
-      createYellow(panel);
-      createWhite(panel);
 
+      createBlue(panel, delegate);
+
+      createGreen(panel, delegate);
+
+      createRed(panel, delegate);
+      //createYellow(panel);
+      //createWhite(panel);
+
+      
+      //add button last, so it is drawn last
+      JButton buttonMove = new JButton("Move Me!");
+      buttonMove.setName("buttonMoveMe");
+      LayoutableAdapterForJComponent buttonMoveLay = configurator.createLayoutableFromSwingPref(buttonMove);
+      buttonMoveLay.getLay().layPoz_MidXToMid_OfParent();
+      buttonMoveLay.getLay().layPoz_MidYToMid_OfParent();
+
+      DragPozerListener listener = new DragPozerListener(slc, buttonMoveLay, panel);
+
+      KeyListenerF4DebugStringable keyLis = new KeyListenerF4DebugStringable(slc, panel);
+
+      buttonMove.addKeyListener(keyLis);
+      buttonMove.addMouseListener(listener);
+      buttonMove.addMouseMotionListener(listener);
+      buttonMove.addMouseWheelListener(listener);
+
+      
+      
       //add listener for computations
       slc.toStringSetDebugBreaks(new LayoutWillListenerAdapter() {
 
@@ -121,19 +178,35 @@ public class RunLayouterDemoSwingDelegate extends RunLayouterDemoSwingAbstract {
 
    }
 
-   private void createBlue(JPanelLayoutable root) {
+   private void createBlue(JPanelLayoutable root, ILayoutDelegate delegate) {
       JPanel panel = new JPanel();
       panel.setName("panelBlue");
       panel.setBackground(Color.BLUE);
       panelBlue = root.addLayoutable(panel);
+
+      Area2DConfigurator lay = panelBlue.getLay();
+
+      lay.layPoz_TopToTop_OfParent();
+      lay.layPoz_EndToEnd_Parent();
+
+      ByteObject sizer = slc.getSizerFactory().getSizerDelegate(delegate);
+      lay.setSizerH(sizer);
+      lay.setSizerW(sizer);
    }
 
-   private void createGreen(JPanelLayoutable root) {
+   private void createGreen(JPanelLayoutable root, ILayoutDelegate delegate) {
       JPanel panel = new JPanel();
       panel.setName("panelGreen");
       panel.setBackground(Color.GREEN);
 
       panelGreen = root.addLayoutable(panel);
+
+      panelGreen.getLay().layPoz_BotToBot_OfParent();
+      panelGreen.getLay().layPoz_EndToEnd_Parent();
+
+      ByteObject sizer = slc.getSizerFactory().getSizerDelegate(delegate);
+      panelGreen.getLay().setSizerH(sizer);
+      panelGreen.getLay().setSizerW(sizer);
    }
 
    private void createOrange(JPanelLayoutable root, ILayoutDelegate delegate) {
@@ -142,23 +215,32 @@ public class RunLayouterDemoSwingDelegate extends RunLayouterDemoSwingAbstract {
       panel.setBackground(Color.ORANGE);
       //first added is on the bottom
       panelOrange = root.addLayoutable(panel);
-      
+
       ByteObject pozerAtPixel0Lazy = slc.getPozerFactory().getPozerAtPixel0Lazy();
-      
+
       panelOrange.getLay().setPozerXStart(pozerAtPixel0Lazy);
       panelOrange.getLay().setPozerYTop(pozerAtPixel0Lazy);
-      
-      ByteObject sizer = slc.getSizerFactory().getSizerListener(delegate);
+
+      ByteObject sizer = slc.getSizerFactory().getSizerDelegate(delegate);
       panelOrange.getLay().setSizerH(sizer);
       panelOrange.getLay().setSizerW(sizer);
 
    }
 
-   private void createRed(JPanelLayoutable root) {
+   private void createRed(JPanelLayoutable root, ILayoutDelegate delegate) {
       JPanel panel = new JPanel();
-      panel.setName("panelYellow");
+      panel.setName("panelRed");
       panel.setBackground(Color.red);
       panelRed = root.addLayoutable(panel);
+
+      Area2DConfigurator lay = panelRed.getLay();
+
+      lay.layPoz_StartToStart_OfParent();
+      lay.layPoz_BotToBot_OfParent();
+
+      ByteObject sizer = slc.getSizerFactory().getSizerDelegate(delegate);
+      lay.setSizerH(sizer);
+      lay.setSizerW(sizer);
    }
 
    private void createWhite(JPanelLayoutable root) {
